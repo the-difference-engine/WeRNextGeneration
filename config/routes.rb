ec4cd2df988aee06e8c656d491bf809992105681
natail <nataleanfuso@gmail.com>
2017-08-07 19:58:04 -0500

    employees/locations mvc and schema image

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
 Rails.application.routes.draw do
 
-# employees routes
-get '/employees' => 'employees#index'
-get '/employees/new' => 'employees#new'
-post '/employees' => 'employees#create'
-get '/employees/:id' => 'employees#show'
-get '/employees/:id/edit' => 'employees#edit'
-patch '/employees/:id' => 'employees#update'
-delete '/employees/:id' => '/employees#destroy'
-
-# location routes
-get '/loctations' => 'locatons#index'
-get '/locations/new' => 'locations#new'
-post '/locations' => 'locations#create'
-get '/locations/:id' => 'locations#show'
-get '/locations/:id/edit' => 'locations#edit'
-patch '/locations/:id' => 'locations#update'
-delete '/locations/:id' => '/locations#destroy'
-
 end

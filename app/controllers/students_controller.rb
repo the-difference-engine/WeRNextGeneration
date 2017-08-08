5a30496a0f3558285b02bd72a5cc26353bd7a073
AddieEli <acadavid71@gmail.com>
2017-08-07 19:28:00 -0500

    added students model

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,29 @@
+class StudentsController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+  end
+
+  def update
+    
+  end
+
+  def destroy 
+  end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get '/students' => 'students#index'
+
+ get '/students/new' => 'students#new'
+ post '/students' => 'students#create'
+
+ get '/students/:id' => 'students#show'
+
+ get '/student/:id/edit' => 'students#edit'
+ patch '/students/:id' => 'students#update'
+
+ delete '/students/:id' => 'students#destroy'
+ 
 end

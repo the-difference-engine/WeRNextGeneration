5b30adaf206ab4fce36eb1c2853b5693aebfccd2
natail <nataleanfuso@gmail.com>
2017-08-07 19:33:16 -0500

    employees and locations models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# employees routes
+get '/employees' => 'employees#index'
+get '/employees/new' => 'employees#new'
+post '/employees' => 'employees#create'
+get '/employees/:id' => 'employees#show'
+get '/employees/:id/edit' => 'employees#edit'
+patch '/employees/:id' => 'employees#update'
+delete '/employees/:id' => '/employees#destroy'
+
+# location routes
+get '/loctations' => 'locatons#index'
+get '/locations/new' => 'locations#new'
+post '/locations' => 'locations#create'
+get '/locations/:id' => 'locations#show'
+get '/locations/:id/edit' => 'locations#edit'
+patch '/locations/:id' => 'locations#update'
+delete '/locations/:id' => '/locations#destroy'
+
 end

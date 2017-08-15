8377c813316d7d0db710ac236b5de8f709da15e5
AddieEli <acadavid71@gmail.com>
2017-08-14 19:42:47 -0500

    added home

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815002318) do
+ActiveRecord::Schema.define(version: 20170815003728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,11 @@ ActiveRecord::Schema.define(version: 20170815002318) do
     t.string "state"
     t.string "zip"
     t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

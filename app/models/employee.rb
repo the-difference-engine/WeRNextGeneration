7b1b8501060b706d22b31ed5fd177a7520fc58a1
natail <nataleanfuso@gmail.com>
2017-08-21 18:10:53 -0500

    associations for employees and locations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ApplicationRecord
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  belongs_to :camp
 end

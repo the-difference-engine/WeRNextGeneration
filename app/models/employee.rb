7d08145fb25e59c19501ad8e13d8b839ce2461ad
natail <nataleanfuso@gmail.com>
2017-08-21 20:37:07 -0500

    table formatting on employee show

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,7 +5,7 @@ class Employee < ApplicationRecord
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-    belongs_to :camp
+    belongs_to :camp, optional: true
     enum category: ["partner", "employee", "admin", "super_admin"]
 
 end

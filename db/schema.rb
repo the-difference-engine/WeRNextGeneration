abc6e6dcc9601260a8698853cf4997016868ff31
“Jose <jguti12@gmail.com>
2017-08-31 20:31:00 -0500

    added locations views and edits to employees views and cleaned up location and employee controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822001430) do
+ActiveRecord::Schema.define(version: 20170901010357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20170822001430) do
     t.string "city"
     t.string "state"
     t.integer "zip"
+    t.boolean "status"
   end
 
   create_table "student_class_lists", force: :cascade do |t|

6a6780cc8a4b9b35a5a753b5f6dc5064879fcac4
victorclee <victorchristopherlee@gmail.com>
2017-09-07 20:48:31 -0500

    updated home page with volunteer sign up sign in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
 
   def index
 
-    redirect_to '/guardians/:id'
-
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908000823) do
+ActiveRecord::Schema.define(version: 20170908003903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,6 +166,18 @@ ActiveRecord::Schema.define(version: 20170908000823) do
     t.datetime "updated_at", null: false
     t.string "country"
     t.string "address_2"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_volunteers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_volunteers_on_reset_password_token", unique: true
   end
 
 end

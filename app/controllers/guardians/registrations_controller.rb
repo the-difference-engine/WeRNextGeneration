243479ce3b15a6fda9b970bb20962e37c8e5f446
KisleK <kisle.kuhn1@gmail.com>
2017-09-15 13:23:51 -0500

    tested styling and routes

--- app/controllers/guardians/registrations_controller.rb
+++ app/controllers/guardians/registrations_controller.rb
@@ -1,31 +1,31 @@
-class Guardians::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+# class Guardians::RegistrationsController < Devise::RegistrationsController
+#   before_action :configure_sign_up_params, only: [:create]
+#   before_action :configure_account_update_params, only: [:update]
 
-  GET /resource/sign_up
-  def new
-    super
-  end
+#   GET /resource/sign_up
+#   def new
+#     super
+#   end
 
-  POST /resource
-  def create
-    super
-  end
+#   POST /resource
+#   def create
+#     super
+#   end
 
-  GET /resource/edit
-  def edit
-    super
-  end
+#   GET /resource/edit
+#   def edit
+#     super
+#   end
 
-  PUT /resource
-  def update
-    super
-  end
+#   PUT /resource
+#   def update
+#     super
+#   end
 
-  DELETE /resource
-  def destroy
-    super
-  end
+#   DELETE /resource
+#   def destroy
+#     super
+#   end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -39,13 +39,13 @@ class Guardians::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   # end
 
   # The path used after sign up.
